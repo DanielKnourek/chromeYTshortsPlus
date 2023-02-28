d9919530f46fae0e514e51d5c1ed95f2b1f6d38a
Daniel Kňourek <knourek.daniel@gmail.com>
2023-02-28 17:21:10 +0100

    demo v0.0.5 - manifest typesafe test

---
+++ src/manifest.ts
@@ -0,0 +1,9 @@
+import { ManifestV3, ManifestV2, chromeExtension } from 'rollup-plugin-chrome-extension'
+
+const mv3: ManifestV3 = {
+    version: "0.0.5",
+    name: 'chrome-present-polarsteps',
+    description: 'Extra 3rd party tools for Polarsteps'
+}
+
+export default mv3
